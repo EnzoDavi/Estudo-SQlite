b0a946dc5658d8f9d488fbf92de561767e4a9fdc
EnzoDavi <161651731+EnzoDavi@users.noreply.github.com>
2026-03-25 13:54:04 -0300

    comentarios SQlite

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Código feitos em python buscando simular um banco de dados com a finalidade de estudar a biblioteca SQlite
+
+(O banco de dados está aberto para públic por não conter nenhuma informação privada e ser apenas paea material de estudo)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+# bibliotecas
 import sqlite3
 
-conexao = sqlite3.connect("banco.db")
-cursor = conexao.cursor()
+# Configurações iniciais de um banco de dados para SQlite3
+conexao = sqlite3.connect("banco.db")   # Nome do arquivo do banco de dados
+cursor = conexao.cursor()               # Cursor do banco de dados
 
+# Criação de um banco de dados (nesse caso com as colunas: id, titular, saldo, cpf)
 cursor.execute("""CREATE TABLE IF NOT EXISTS banco (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 titular TEXT NOT NULL,
@@ -10,31 +13,32 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS banco (
                 cpf TEXT NOT NULL UNIQUE)
                 """)
 
+# Inserir dados no banco
 cursor.execute(f"""INSERT INTO banco
                 (titular, saldo, cpf) VALUES
                 ('{input("Seu nome: ")}',{input("Seu saldo atual: ")}, '{input("Seu CPF: ")}')
                 """)
 
+# Consulta geral e inprimir retorno
 cursor.execute("""SELECT * FROM banco""")
-
 banco = cursor.fetchall()
 print(banco)
 
+# Consulta com filtro
 cursor.execute("""SELECT titular,saldo FROM banco
                 WHERE saldo > 100""")
-
 ContasMaior100 = cursor.fetchall()
 print(ContasMaior100)
 
+# Deletar dados do banco
 cursor.execute("""DELETE FROM banco WHERE saldo = 0""")
 
+# Atualizar dados 
 cursor.execute("""UPDATE banco
                 SET saldo = saldo - 100
                 WHERE titular = 'Enzo'""")
-
 cursor.execute("""SELECT * FROM banco""")
-
 print(cursor.fetchall())
 
-
+# Enviar atualizações feitas
 conexao.commit()
